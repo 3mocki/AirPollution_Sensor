ceab482298b4664d28eb00d37e99c8b1e7a760d6
3mocki <starleesm@gmail.com>
2018-11-13 21:04:42 -0800

    Uncompleted Code_8

--- globalVar.py
+++ globalVar.py
@@ -1,9 +1,7 @@
-global ssn, cId, MTI, TTI
-
 header_size = 6
-EDS = 255
-AQLT=1
-EAQDLT=2
+EDS = 255 #EXTENDED_DATA_SIZE
+AQLT=1 #AIR_QUALITY_DATA_LIST_TYPE
+EAQDLT=2 #EXTENDED_AIR_QUALITY_DATA_LIST_TYPE
 
 NULL_VALUE = ""
 RES_FAILED = 0
@@ -12,4 +10,9 @@ TIMESTAMP_SIZE = 4
 
 url_1 = 'http://dev.qualcomminst.com:8080/serverapi'
 url_2 = 'http://dev.qualcomminst.com:8080/serverdatatran'
-# url_1 = 'http://dev.qualcomminst.com:3500/serverapi'
+# url_1 = 'http://dev.qualcomminst.com:3500/serverapi' #For Simulator
+
+geo_1 = '32.882598,-117.234821'
+geo_2 = 'Q30'
+geo_3 = 'Q99'
+geo_4 = 'Q16552'
